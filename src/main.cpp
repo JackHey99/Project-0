508cf36da12ba53f6d16174fa20aa70c7aba3b13
JackHey99 <jackheyward5500@gmail.com>
2019-08-29 12:15:53 +1200

    Working Code. Needs to be cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
-#include <Arduino.h>
+#include <arduino.h>
 
 //Configuration
 uint8_t BUTTON_PIN = 14;
-uint8_t LED_PIN = 12;
-uint32_t debouncedelay_ms = 100;
+uint8_t led_pin = 13;
+uint32_t debouncedelay_ms = 120;
 uint8_t POT_PIN = A1;
 //----------------------------------//
 
 //Initilize variable values
-//uint32_t pot_value = 0;
-//float pot_voltage = 0;
 volatile bool button_pressed = 0;
+uint32_t delta_t_ms = 0;
+float frequancy = 0;
 //----------------------------------//
 
 void ISR_button_pressed();
@@ -19,22 +19,52 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(led_pin, OUTPUT);
   attachInterrupt(BUTTON_PIN, ISR_button_pressed, FALLING);
 }
 
 void loop()
 {
+
+//Configure Local Variables 
   uint32_t pot_value = analogRead(POT_PIN);
-  float pot_voltage = (pot_value * 3.3) / (1023);
+  delta_t_ms = map(pot_value, 0, 1023, 3 , 30);
+  static bool led_state = 0;
+  static uint32_t previoustime_ms = 0;
+  uint32_t currenttime_ms = millis();
+
+
+
+  if ((currenttime_ms - previoustime_ms) >= delta_t_ms)
+  {
+    if (led_state == 0)
+    {
+      led_state = 1;
+    }
+    else
+    {
+      led_state = 0;
+    }
+    digitalWrite(led_pin, led_state);
+    previoustime_ms = currenttime_ms;
+  }
 
   if (button_pressed == true)
   {
-    Serial.println(pot_voltage, 4);
+    float pwm_period_ms = delta_t_ms;
+    float frequancy = (1000 / (pwm_period_ms * 2));
+    
+    
+    Serial.print("Your threshold is:  ");
+    Serial.print((frequancy), 3);
+    Serial.println("Hz");
+
   }
 
   button_pressed = false;
 }
+
+
 
 void ISR_button_pressed()
 {
